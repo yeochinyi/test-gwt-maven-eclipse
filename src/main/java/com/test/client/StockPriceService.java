24c3a529e044a4b06b6e65324a60a99ab2aec2bd
yeochinyi <yeochinyi@gmail.com>
2013-03-07 19:07:14 +0800

    initial version

---
+++ src/main/java/com/test/client/StockPriceService.java
@@ -0,0 +1,10 @@
+package com.test.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+@RemoteServiceRelativePath("stockPrices")
+public interface StockPriceService extends RemoteService {
+
+  StockPrice[] getPrices(String[] symbols) throws DelistedException;
+}
